4cbb5f143d601795acd82b8ac39f7d54e7d9dd7a
malefor2 <malefor22@gmail.com>
2017-02-13 19:37:21 +0100

    f

--- slider.js
+++ slider.js
@@ -7,5 +7,6 @@ function slide(num){
 
         var positions = ["0", w*(-1)+"px", w*(-2)+"px"];
         var translate = "translate3d("+ positions[num] +", 0px, 0px)";
+
         wrapper.style.transform = translate;
 }
